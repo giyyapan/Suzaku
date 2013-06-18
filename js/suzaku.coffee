7c0c8427a522c903fd0790b91d8dcdc048d18dd6
giyyapan <giyyapan@gmail.com>
2013-06-18 11:35:01 +0800

    add libs

---
+++ js/suzaku.coffee
@@ -0,0 +1,324 @@
+if not $
+  console.warn "cannot find JQuery!  -- Suzaku"
+  
+class Suzaku
+  constructor:()->
+    console.log "init Suzaku" 
+    @Widget = Widget
+    @TemplateManager = TemplateManager
+    @EventEmitter = EventEmitter
+    @AjaxManager = AjaxManager
+    
+    @KeybordManager = null    
+    @AnimationManager = null
+    @Utils = null
+    @Key = null
+    @Mouse = null
+    @WsServer = null
+    
+
+class EventEmitter
+  constructor:()->
+    @_events = {}
+  on:(event,callback)->
+    @_events[event] = [] if not @_events[event]
+    @_events[event].push callback
+  off:(event)->
+    for func in @_events[event]
+      func = null
+    delete @_events[event]
+  emit:(event,data)->
+    return if !@_events[event]
+    for func in @_events[event]
+      func(data)
+      
+class Widget extends EventEmitter
+  constructor:(creator)->
+    if not creator
+      console.error "need a creator! -- Suzaku.Widget"
+      return
+    @J = null
+    @dom = null
+    @template = null
+    @creator = creator
+    @UI = []
+    if creator instanceof String
+      if creator.indexOf("<")> -1 and creator.indexOf(">")>-1
+        template = creator
+        tempDiv = document.createElement "div"
+        tempDiv.innerHtml = template
+        @dom = template.children[0]
+        @J = $ @dom if $
+      else
+        @J = $ creator if $
+        @dom = document.queryCreator creator
+        if @dom.length is 0
+          console.error "Wrong selector! cannot find element by this -- Suzaku.Widget"
+          return
+    if $ and creator instanceof $
+      @J = creator
+      @dom = @J[0]
+    if creator instanceof window.HTMLElement
+      @dom = creator
+      @J = $ dom if $
+    @initUI()
+  initUI:()->
+    for dom in @dom.children
+      did = dom.getAttribute "data-id"
+      if did then @UI[did] =
+        dom:dom
+        J:if $ then $ dom else null
+  remove:()->
+    @dom.parentElement.removeChild @dom
+  before:(target)->
+    if target instanceof Widget
+      target = target.dom
+    if $ and target instanceof $
+      target = target[0]
+    if target instanceof HTMLElement
+      target.parentElement.insertBefore @dom,target
+    else
+      console.error "invaild target!  --Suzaku.Widget"
+  after:(target)->
+    if target instanceof Widget
+      target = target.dom
+    if $ and target instanceof $
+      target = target[0]
+    if target instanceof HTMLElement
+      parent = target.parentElement
+      next = null
+      for dom,index in parent.children 
+        if dom is target and index < parent.children.length - 1
+          next = parent.children[index+1]
+      if next then parent.insertBefore @dom,next
+      else parent.appendChild @dom
+    else
+      console.error "invaild target!  --Suzaku.Widget"
+  replace:(target)->
+    @before target
+    if target instanceof Widget then target.remove()
+    if target instanceof HTMLElement then target.parentElement.removeChild target
+          
+class TemplateManager extends EventEmitter
+  constructor:()->
+    @templates = {}
+    @tplNames = []
+  use:()->
+    for item in arguments
+      @tplNames.push item
+  start:()->
+    ajaxManager = new AjaxManager
+    loaclDir = "../templates/"
+    for name in tplNames
+      url = localDir+name
+      req = ajaxManager.addGetRequest url,null,(data,textStatus,req)=>
+        @templates[req.Suzaku_tplName] = data
+      req.Suzaku_tplName = name
+    ajaxManager.start =>
+      @emit.onload
+
+class AjaxManager extends EventEmitter
+  constructor:()->
+    return console.error "ajax Manager needs Jquery!" if not $
+    @reqs = []
+    @ajaxTasks = {}
+    @tidCounter = 0
+  addRequest:(option)->
+    option.type = option.type or 'get'
+    return console.error "ajax need url!" if not option.url
+    console.log "Add request:",option.type,"to",option.url,"--Suzaku.AjaxManager"
+    option.externPort = {}
+    @reqs.push option
+    return option.externPort
+  addGetRequest:(url,data,success,dataType)->
+    return @addRequest
+      type:"get"
+      url:url
+      data:data
+      dataType:dataType
+      success:success
+  addPostRequest:(url,data,success,dataType)->
+    return @addRequest
+      type:"post"
+      url:url
+      data:data
+      dataType:dataType
+      success:success
+  start:(callback)->
+    id = @tidCounter += 1
+    newAjaxTask = 
+      id:id
+      reqs:@reqs
+      finishedNum:0
+    @ajaxTasks[id] = newAjaxTask
+    console.log "start request tasks",newAjaxTask
+    @reqs = []
+    ajaxManager = this
+    for request,index in @ajaxTasks[id]
+      option = request.option
+      ajaxOpt = Utils.copy option
+      delete ajaxOpt.retry
+      delete ajaxOpt.externPort
+      ajaxOpt.success = (data,textStatus,req)=>
+        @_ajaxSuccess.apply req,arguments
+      ajaxOpt.error = (req,textStatus,error)=>
+        @_ajaxError.apply req,arguments
+        
+      ajaxReq = $.ajax ajaxOpt
+      ajaxReq.Suzaku_ajaxOpt = ajaxOpt
+      ajaxReq.Suzaku_taskOpt = option
+      ajaxReq.Suzaku_ajaxTask = newAjaxTask
+      ajaxReq[name] = value for name,value of option.externPort
+
+      
+    @on "finish",(taskId)=>
+      delete @ajaxTasks[taskId]
+      callback if typeof callback is "function"
+      
+  _ajaxSuccess:(data,textStatus,req)->
+    ajaxTask =  req.Suzaku_ajaxTask
+    ajaxTask.finishedNum += 1
+    ajaxTask.Suzaku_taskOpt.success data,textStatus,req if ajaxTask.Suzaku_taskOpt.success
+    if ajaxTask.finishedNum is ajaxTask.reqs.length
+      @emit "finish",ajaxTask.id
+        
+  _ajaxError:(req,textStatus,error)->
+    taskOpt = req.Suzaku_taskOpt
+    retry = taskOpt.retry
+    retried = req.Suzaku_retried or 0
+    if retried < retry
+      ajaxReq = $.ajax req.Suzaku_ajaxOpt
+      ajaxReq.Suzaku_ajaxOpt = req.Suzaku_ajaxOpt
+      ajaxReq.Suzaku_taskOpt = req.Suzaku_taskOpt
+      ajaxReq.Suzaku_ajaxTask = req.Suzaku_ajaxTask
+      ajaxReq[name] = value for name,value of req.Suzaku_taskOpt.externPort
+      ajaxReq.Suzaku_retried = retried + 1
+    else
+      ajaxTask = req.Suzaku_ajaxTask
+      ajaxTask.finishedNum += 1
+      console.error "request failed!",req,textStatus,error
+      ajaxTask.Suzaku_taskOpt.fail req,textStatus,error if ajaxTask.Suzaku_taskOpt.fail
+      if ajaxTask.finishedNum is ajaxTask.reqs.length
+        @emit "finish",ajaxTask.id
+      
+
+window.Suzaku = new Suzaku
+window.Suzaku.Utils =
+  clone:(target,deepClone=false)->
+    if target instanceof Array
+      newArr = []
+      for item,index in target
+        newArr[index] = if deepClone then Utils.clone item,true else item
+      return newArr
+    if typeof target is 'object'
+      newObj = {}
+      for name,item of target
+        newObj[name] = if deepClone then Utils.clone item,true else item
+      return newObj
+    return target
+    
+  compare:(a,b)->
+    if a is b then return true
+    if typeof a is "number" and typeof b is "number"
+      if Math.abs(a-b) < 0.0001 then return true
+    if typeof a is "object" and typeof b is "object"
+      for name,value of a
+        if not Utils.compare b[name],value then return false
+      for name,value of b
+        if not Utils.compare a[name],value then return false
+      return true
+    return false
+    
+  removeItem:(source,target)->
+    if source instanceof Array and typeof target is 'number'
+      return source.splice target,1
+      
+    if typeof source is 'object'
+      if typeof target is 'string' or typeof target is 'number'
+        return delete source[target]
+        
+      if typeof target is 'object'
+        for name,item of source
+          if item is target then return Utils.removeItem source,name
+          found = true
+          for keyname,keyvalue of target
+            if Utils.compare item[keyname],keyvalue
+              continue
+            else
+              found = false
+              break
+          if found then return Utils.removeItem source,name
+          
+  findItem:(source,key,value)->
+    if typeof key is 'string' and typeof value isnt 'undefined'
+      target = {key:value}
+    else target = key
+    for name,item of source
+      if item is target then return {target:target,index:null}
+      found = true
+      for keyname,keyvalue of target
+        if Utils.compare item[keyname],keyvalue
+          continue
+        else
+          found = false
+          break
+      if found then return {target:target,index:name}
+        
+window.Suzaku.Key =
+  0:48
+  1:49
+  2:50
+  3:51
+  4:52
+  5:53
+  6:54
+  7:55
+  8:56
+  9:57
+  a:65
+  b:66
+  c:67
+  d:68
+  e:69
+  f:70
+  g:71
+  h:72
+  i:73
+  j:74
+  k:75
+  l:76
+  m:77
+  n:78
+  o:79
+  p:80
+  q:81
+  r:82
+  s:83
+  t:84
+  u:85
+  v:86
+  w:87
+  x:88
+  y:89
+  z:90
+  space:32
+  shift:16
+  ctrl:17
+  alt:18
+  left:37
+  right:39
+  down:40
+  enter:13
+  backspace:8
+  escape:27
+  del:46
+  esc:27
+  pageup:33
+  pagedown:34
+  tab:9
+window.Suzaku.Mouse = 
+  left:0
+  middle:1
+  right:2
+
+
---
+++ js/suzaku.js
@@ -0,0 +1,554 @@
+(function() {
+  var AjaxManager, EventEmitter, Suzaku, TemplateManager, Widget,
+    __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+  if (!$) {
+    console.warn("cannot find JQuery!  -- Suzaku");
+  }
+
+  Suzaku = (function() {
+
+    function Suzaku() {
+      console.log("init Suzaku");
+      this.Widget = Widget;
+      this.TemplateManager = TemplateManager;
+      this.EventEmitter = EventEmitter;
+      this.AjaxManager = AjaxManager;
+      this.KeybordManager = null;
+      this.AnimationManager = null;
+      this.Utils = null;
+      this.Key = null;
+      this.Mouse = null;
+      this.WsServer = null;
+    }
+
+    return Suzaku;
+
+  })();
+
+  EventEmitter = (function() {
+
+    function EventEmitter() {
+      this._events = {};
+    }
+
+    EventEmitter.prototype.on = function(event, callback) {
+      if (!this._events[event]) {
+        this._events[event] = [];
+      }
+      return this._events[event].push(callback);
+    };
+
+    EventEmitter.prototype.off = function(event) {
+      var func, _i, _len, _ref;
+      _ref = this._events[event];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        func = _ref[_i];
+        func = null;
+      }
+      return delete this._events[event];
+    };
+
+    EventEmitter.prototype.emit = function(event, data) {
+      var func, _i, _len, _ref, _results;
+      if (!this._events[event]) {
+        return;
+      }
+      _ref = this._events[event];
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        func = _ref[_i];
+        _results.push(func(data));
+      }
+      return _results;
+    };
+
+    return EventEmitter;
+
+  })();
+
+  Widget = (function(_super) {
+
+    __extends(Widget, _super);
+
+    function Widget(creator) {
+      var tempDiv, template;
+      if (!creator) {
+        console.error("need a creator! -- Suzaku.Widget");
+        return;
+      }
+      this.J = null;
+      this.dom = null;
+      this.template = null;
+      this.creator = creator;
+      this.UI = [];
+      if (creator instanceof String) {
+        if (creator.indexOf("<") > -1 && creator.indexOf(">") > -1) {
+          template = creator;
+          tempDiv = document.createElement("div");
+          tempDiv.innerHtml = template;
+          this.dom = template.children[0];
+          if ($) {
+            this.J = $(this.dom);
+          }
+        } else {
+          if ($) {
+            this.J = $(creator);
+          }
+          this.dom = document.queryCreator(creator);
+          if (this.dom.length === 0) {
+            console.error("Wrong selector! cannot find element by this -- Suzaku.Widget");
+            return;
+          }
+        }
+      }
+      if ($ && creator instanceof $) {
+        this.J = creator;
+        this.dom = this.J[0];
+      }
+      if (creator instanceof window.HTMLElement) {
+        this.dom = creator;
+        if ($) {
+          this.J = $(dom);
+        }
+      }
+      this.initUI();
+    }
+
+    Widget.prototype.initUI = function() {
+      var did, dom, _i, _len, _ref, _results;
+      _ref = this.dom.children;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        dom = _ref[_i];
+        did = dom.getAttribute("data-id");
+        if (did) {
+          _results.push(this.UI[did] = {
+            dom: dom,
+            J: $ ? $(dom) : null
+          });
+        } else {
+          _results.push(void 0);
+        }
+      }
+      return _results;
+    };
+
+    Widget.prototype.remove = function() {
+      return this.dom.parentElement.removeChild(this.dom);
+    };
+
+    Widget.prototype.before = function(target) {
+      if (target instanceof Widget) {
+        target = target.dom;
+      }
+      if ($ && target instanceof $) {
+        target = target[0];
+      }
+      if (target instanceof HTMLElement) {
+        return target.parentElement.insertBefore(this.dom, target);
+      } else {
+        return console.error("invaild target!  --Suzaku.Widget");
+      }
+    };
+
+    Widget.prototype.after = function(target) {
+      var dom, index, next, parent, _i, _len, _ref;
+      if (target instanceof Widget) {
+        target = target.dom;
+      }
+      if ($ && target instanceof $) {
+        target = target[0];
+      }
+      if (target instanceof HTMLElement) {
+        parent = target.parentElement;
+        next = null;
+        _ref = parent.children;
+        for (index = _i = 0, _len = _ref.length; _i < _len; index = ++_i) {
+          dom = _ref[index];
+          if (dom === target && index < parent.children.length - 1) {
+            next = parent.children[index + 1];
+          }
+        }
+        if (next) {
+          return parent.insertBefore(this.dom, next);
+        } else {
+          return parent.appendChild(this.dom);
+        }
+      } else {
+        return console.error("invaild target!  --Suzaku.Widget");
+      }
+    };
+
+    Widget.prototype.replace = function(target) {
+      this.before(target);
+      if (target instanceof Widget) {
+        target.remove();
+      }
+      if (target instanceof HTMLElement) {
+        return target.parentElement.removeChild(target);
+      }
+    };
+
+    return Widget;
+
+  })(EventEmitter);
+
+  TemplateManager = (function(_super) {
+
+    __extends(TemplateManager, _super);
+
+    function TemplateManager() {
+      this.templates = {};
+      this.tplNames = [];
+    }
+
+    TemplateManager.prototype.use = function() {
+      var item, _i, _len, _results;
+      _results = [];
+      for (_i = 0, _len = arguments.length; _i < _len; _i++) {
+        item = arguments[_i];
+        _results.push(this.tplNames.push(item));
+      }
+      return _results;
+    };
+
+    TemplateManager.prototype.start = function() {
+      var ajaxManager, loaclDir, name, req, url, _i, _len,
+        _this = this;
+      ajaxManager = new AjaxManager;
+      loaclDir = "../templates/";
+      for (_i = 0, _len = tplNames.length; _i < _len; _i++) {
+        name = tplNames[_i];
+        url = localDir + name;
+        req = ajaxManager.addGetRequest(url, null, function(data, textStatus, req) {
+          return _this.templates[req.Suzaku_tplName] = data;
+        });
+        req.Suzaku_tplName = name;
+      }
+      return ajaxManager.start(function() {
+        return _this.emit.onload;
+      });
+    };
+
+    return TemplateManager;
+
+  })(EventEmitter);
+
+  AjaxManager = (function(_super) {
+
+    __extends(AjaxManager, _super);
+
+    function AjaxManager() {
+      if (!$) {
+        return console.error("ajax Manager needs Jquery!");
+      }
+      this.reqs = [];
+      this.ajaxTasks = {};
+      this.tidCounter = 0;
+    }
+
+    AjaxManager.prototype.addRequest = function(option) {
+      option.type = option.type || 'get';
+      if (!option.url) {
+        return console.error("ajax need url!");
+      }
+      console.log("Add request:", option.type, "to", option.url, "--Suzaku.AjaxManager");
+      option.externPort = {};
+      this.reqs.push(option);
+      return option.externPort;
+    };
+
+    AjaxManager.prototype.addGetRequest = function(url, data, success, dataType) {
+      return this.addRequest({
+        type: "get",
+        url: url,
+        data: data,
+        dataType: dataType,
+        success: success
+      });
+    };
+
+    AjaxManager.prototype.addPostRequest = function(url, data, success, dataType) {
+      return this.addRequest({
+        type: "post",
+        url: url,
+        data: data,
+        dataType: dataType,
+        success: success
+      });
+    };
+
+    AjaxManager.prototype.start = function(callback) {
+      var ajaxManager, ajaxOpt, ajaxReq, id, index, name, newAjaxTask, option, request, value, _i, _len, _ref, _ref1,
+        _this = this;
+      id = this.tidCounter += 1;
+      newAjaxTask = {
+        id: id,
+        reqs: this.reqs,
+        finishedNum: 0
+      };
+      this.ajaxTasks[id] = newAjaxTask;
+      console.log("start request tasks", newAjaxTask);
+      this.reqs = [];
+      ajaxManager = this;
+      _ref = this.ajaxTasks[id];
+      for (index = _i = 0, _len = _ref.length; _i < _len; index = ++_i) {
+        request = _ref[index];
+        option = request.option;
+        ajaxOpt = Utils.copy(option);
+        delete ajaxOpt.retry;
+        delete ajaxOpt.externPort;
+        ajaxOpt.success = function(data, textStatus, req) {
+          return _this._ajaxSuccess.apply(req, arguments);
+        };
+        ajaxOpt.error = function(req, textStatus, error) {
+          return _this._ajaxError.apply(req, arguments);
+        };
+        ajaxReq = $.ajax(ajaxOpt);
+        ajaxReq.Suzaku_ajaxOpt = ajaxOpt;
+        ajaxReq.Suzaku_taskOpt = option;
+        ajaxReq.Suzaku_ajaxTask = newAjaxTask;
+        _ref1 = option.externPort;
+        for (name in _ref1) {
+          value = _ref1[name];
+          ajaxReq[name] = value;
+        }
+      }
+      return this.on("finish", function(taskId) {
+        delete _this.ajaxTasks[taskId];
+        if (typeof callback === "function") {
+          return callback;
+        }
+      });
+    };
+
+    AjaxManager.prototype._ajaxSuccess = function(data, textStatus, req) {
+      var ajaxTask;
+      ajaxTask = req.Suzaku_ajaxTask;
+      ajaxTask.finishedNum += 1;
+      if (ajaxTask.Suzaku_taskOpt.success) {
+        ajaxTask.Suzaku_taskOpt.success(data, textStatus, req);
+      }
+      if (ajaxTask.finishedNum === ajaxTask.reqs.length) {
+        return this.emit("finish", ajaxTask.id);
+      }
+    };
+
+    AjaxManager.prototype._ajaxError = function(req, textStatus, error) {
+      var ajaxReq, ajaxTask, name, retried, retry, taskOpt, value, _ref;
+      taskOpt = req.Suzaku_taskOpt;
+      retry = taskOpt.retry;
+      retried = req.Suzaku_retried || 0;
+      if (retried < retry) {
+        ajaxReq = $.ajax(req.Suzaku_ajaxOpt);
+        ajaxReq.Suzaku_ajaxOpt = req.Suzaku_ajaxOpt;
+        ajaxReq.Suzaku_taskOpt = req.Suzaku_taskOpt;
+        ajaxReq.Suzaku_ajaxTask = req.Suzaku_ajaxTask;
+        _ref = req.Suzaku_taskOpt.externPort;
+        for (name in _ref) {
+          value = _ref[name];
+          ajaxReq[name] = value;
+        }
+        return ajaxReq.Suzaku_retried = retried + 1;
+      } else {
+        ajaxTask = req.Suzaku_ajaxTask;
+        ajaxTask.finishedNum += 1;
+        console.error("request failed!", req, textStatus, error);
+        if (ajaxTask.Suzaku_taskOpt.fail) {
+          ajaxTask.Suzaku_taskOpt.fail(req, textStatus, error);
+        }
+        if (ajaxTask.finishedNum === ajaxTask.reqs.length) {
+          return this.emit("finish", ajaxTask.id);
+        }
+      }
+    };
+
+    return AjaxManager;
+
+  })(EventEmitter);
+
+  window.Suzaku = new Suzaku;
+
+  window.Suzaku.Utils = {
+    clone: function(target, deepClone) {
+      var index, item, name, newArr, newObj, _i, _len;
+      if (deepClone == null) {
+        deepClone = false;
+      }
+      if (target instanceof Array) {
+        newArr = [];
+        for (index = _i = 0, _len = target.length; _i < _len; index = ++_i) {
+          item = target[index];
+          newArr[index] = deepClone ? Utils.clone(item, true) : item;
+        }
+        return newArr;
+      }
+      if (typeof target === 'object') {
+        newObj = {};
+        for (name in target) {
+          item = target[name];
+          newObj[name] = deepClone ? Utils.clone(item, true) : item;
+        }
+        return newObj;
+      }
+      return target;
+    },
+    compare: function(a, b) {
+      var name, value;
+      if (a === b) {
+        return true;
+      }
+      if (typeof a === "number" && typeof b === "number") {
+        if (Math.abs(a - b) < 0.0001) {
+          return true;
+        }
+      }
+      if (typeof a === "object" && typeof b === "object") {
+        for (name in a) {
+          value = a[name];
+          if (!Utils.compare(b[name], value)) {
+            return false;
+          }
+        }
+        for (name in b) {
+          value = b[name];
+          if (!Utils.compare(a[name], value)) {
+            return false;
+          }
+        }
+        return true;
+      }
+      return false;
+    },
+    removeItem: function(source, target) {
+      var found, item, keyname, keyvalue, name;
+      if (source instanceof Array && typeof target === 'number') {
+        return source.splice(target, 1);
+      }
+      if (typeof source === 'object') {
+        if (typeof target === 'string' || typeof target === 'number') {
+          return delete source[target];
+        }
+        if (typeof target === 'object') {
+          for (name in source) {
+            item = source[name];
+            if (item === target) {
+              return Utils.removeItem(source, name);
+            }
+            found = true;
+            for (keyname in target) {
+              keyvalue = target[keyname];
+              if (Utils.compare(item[keyname], keyvalue)) {
+                continue;
+              } else {
+                found = false;
+                break;
+              }
+            }
+            if (found) {
+              return Utils.removeItem(source, name);
+            }
+          }
+        }
+      }
+    },
+    findItem: function(source, key, value) {
+      var found, item, keyname, keyvalue, name, target;
+      if (typeof key === 'string' && typeof value !== 'undefined') {
+        target = {
+          key: value
+        };
+      } else {
+        target = key;
+      }
+      for (name in source) {
+        item = source[name];
+        if (item === target) {
+          return {
+            target: target,
+            index: null
+          };
+        }
+        found = true;
+        for (keyname in target) {
+          keyvalue = target[keyname];
+          if (Utils.compare(item[keyname], keyvalue)) {
+            continue;
+          } else {
+            found = false;
+            break;
+          }
+        }
+        if (found) {
+          return {
+            target: target,
+            index: name
+          };
+        }
+      }
+    }
+  };
+
+  window.Suzaku.Key = {
+    0: 48,
+    1: 49,
+    2: 50,
+    3: 51,
+    4: 52,
+    5: 53,
+    6: 54,
+    7: 55,
+    8: 56,
+    9: 57,
+    a: 65,
+    b: 66,
+    c: 67,
+    d: 68,
+    e: 69,
+    f: 70,
+    g: 71,
+    h: 72,
+    i: 73,
+    j: 74,
+    k: 75,
+    l: 76,
+    m: 77,
+    n: 78,
+    o: 79,
+    p: 80,
+    q: 81,
+    r: 82,
+    s: 83,
+    t: 84,
+    u: 85,
+    v: 86,
+    w: 87,
+    x: 88,
+    y: 89,
+    z: 90,
+    space: 32,
+    shift: 16,
+    ctrl: 17,
+    alt: 18,
+    left: 37,
+    right: 39,
+    down: 40,
+    enter: 13,
+    backspace: 8,
+    escape: 27,
+    del: 46,
+    esc: 27,
+    pageup: 33,
+    pagedown: 34,
+    tab: 9
+  };
+
+  window.Suzaku.Mouse = {
+    left: 0,
+    middle: 1,
+    right: 2
+  };
+
+}).call(this);
